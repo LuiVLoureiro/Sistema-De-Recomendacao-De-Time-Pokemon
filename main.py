81e4643efb9a1997561fef9387c7428ca261e5f4
LuiLoureiro <luivloureiro@gmail.com>
2024-04-12 08:08:10 -0300

    ftr: Scrapping de Dados da Página Oficial

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+
+# Scrapping
+url = requests.get('https://pokemondb.net/pokedex/all')
+soup = BeautifulSoup(url.text, 'html.parser')
+
+# Filtro para achar as informações na página
+lista_pokemon_nome = soup.find_all('a', class_='ent-name')
+lista_pokemon_tipos = soup.find_all('td', class_='cell-icon')
+lista_pokemon_status = soup.find_all('td', class_='cell-num')
