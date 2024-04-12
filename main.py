03d0d63c8f7c164934dd7b5e5e536c37a4202ee8
LuiLoureiro <luivloureiro@gmail.com>
2024-04-12 08:14:06 -0300

    ftr: Filtrar informações

--- main.py
+++ main.py
@@ -11,3 +11,5 @@ soup = BeautifulSoup(url.text, 'html.parser')
 lista_pokemon_nome = soup.find_all('a', class_='ent-name')
 lista_pokemon_tipos = soup.find_all('td', class_='cell-icon')
 lista_pokemon_status = soup.find_all('td', class_='cell-num')
+
+print(lista_pokemon_nome)
